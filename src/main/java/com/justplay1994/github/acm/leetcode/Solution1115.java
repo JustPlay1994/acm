72249ca3c940b508abd88d3bd5208c80f9de0245
huangzezhou <a4988093>
2020-07-13 10:33:36 +0800

    mutex

--- src/main/java/com/justplay1994/github/acm/leetcode/Solution1115.java
+++ src/main/java/com/justplay1994/github/acm/leetcode/Solution1115.java
@@ -5,91 +5,58 @@ package com.justplay1994.github.acm.leetcode;
  * 1115. 交替打印FooBar
  */
 public class Solution1115 {
-    static
+    /**
+     * 1. mutex（自己实现的太慢，5%）
+     * 2. volatile + cas + 自旋（不行，因为检测锁和上锁是两个步骤，多线程下不安全）
+     * 3. volatile + 自旋 （不行，因为多个线程，可能会同时冲破临界区，并且可能会导致永久等待。
+     */
     class FooBar {
-        int lock_1 = 1;
-        int lock_2 = 0;
         private int n;
 
-        final Object lock = new Object();
+        class Mutex{
+            volatile private boolean isLocked = false;
+
+            public synchronized void lock() throws InterruptedException{
+                while( isLocked ){
+                    this.wait();
+                }
+                isLocked = true;
+            }
+
+            public synchronized void unlock(){
+                isLocked = false;
+                this.notifyAll();
+            }
+        }
+
+        Mutex a = new Mutex();
+        Mutex b = new Mutex();
 
         public FooBar(int n) {
             this.n = n;
+            try {
+                b.lock();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
         public void foo(Runnable printFoo) throws InterruptedException {
             for (int i = 0; i < n; i++) {
-                while(lock_1 == 0){
-                    synchronized (this){
-                        wait();
-                    }
-                }
-                lock_1 = 0;
+                a.lock();
                 // printFoo.run() outputs "foo". Do not change or remove this line.
                 printFoo.run();
-                lock_2 = 1;
-
-                synchronized (this){
-                    notify();
-                }
-
+                b.unlock();
             }
         }
 
         public void bar(Runnable printBar) throws InterruptedException {
             for (int i = 0; i < n; i++) {
-                while (lock_2 == 0){
-                    synchronized (this){
-                        wait();
-                    }
-                }
-                lock_2 = 0;
+                b.lock();
                 // printBar.run() outputs "bar". Do not change or remove this line.
                 printBar.run();
-                lock_1 = 1;
-
-                synchronized (this){
-                    notify();
-                }
+                a.unlock();
             }
         }
-    }
-
-    public static void main(String[] args){
-        Runnable printFoo = new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("foo");
-            }
-        };
-        Runnable printBar = new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("bar");
-            }
-        };
-        FooBar fooBar = new FooBar(3);
-        Thread t1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    fooBar.foo(printFoo);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        Thread t2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    fooBar.bar(printBar);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        t1.start();
-        t2.start();
     }
 }
